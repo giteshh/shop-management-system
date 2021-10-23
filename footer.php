0cbd5d556f67895118891a3086cc48e65facd381
giteshh <90863346+giteshh@users.noreply.github.com>
2021-10-23 13:48:20 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,34 @@
+<!-- partial:partials/_footer.html -->
+<footer class="footer">
+          <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2018
+              <a href="http://www.bootstrapdash.com/" target="_blank">Bootstrapdash</a>. All rights reserved.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with
+              <i class="mdi mdi-heart text-danger"></i>
+            </span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+  <!-- plugins:js -->
+  <script src="https://omo24x7.github.io/cdn/sadmin/vendors/js/vendor.bundle.base.js"></script>
+  <script src="https://omo24x7.github.io/cdn/sadmin/vendors/js/vendor.bundle.addons.js"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="https://omo24x7.github.io/cdn/sadmin/js/off-canvas.js"></script>
+  <script src="https://omo24x7.github.io/cdn/sadmin/js/misc.js"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="https://omo24x7.github.io/cdn/sadmin/js/dashboard.js"></script>
+  <!-- End custom js for this page-->
+</body>
+
+</html>
